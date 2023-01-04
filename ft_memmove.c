c4446da26166e910538bb9364e21ad319f8a6bb7
Felix Von nagel <fvon-nag@c2s16d93.42wolfsburg.de>
2023-01-04 12:06:15 +0100

    memmove works now

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,12 +6,13 @@
 /*   By: fvon-nag <fvon-nag@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 09:14:48 by fvon-nag          #+#    #+#             */
-/*   Updated: 2022/12/20 11:28:40 by fvon-nag         ###   ########.fr       */
+/*   Updated: 2023/01/04 12:00:33 by fvon-nag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 #include <stdlib.h>
+#include "libft.h"
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
@@ -19,25 +20,21 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	char	*csrc;
 	char	*cdest;
 
+	if (src == NULL && n != 0 && dest == 0)
+		return (NULL);
 	csrc = (char *) src;
 	cdest = (char *) dest;
 	i = 0;
-	if (src + n >= dest)
+	if (dest < src)
 	{
+		ft_memcpy(dest, src, n);
+		return (dest);
+	}
 		i = n - 1;
-		while (i >= 0)
-		{
+	while (i >= 0)
+	{
 			cdest[i] = csrc[i];
 			i--;
-		}
-	}
-	else
-	{
-		while (i < (int) n)
-		{
-		cdest[i] = csrc[i];
-		i++;
-		}
 	}
 	return (dest);
 }
