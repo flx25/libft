2c6d8fc62db0095dfe7734fd9c65795e995b0b79
Felix Von nagel <fvon-nag@c2s16d93.42wolfsburg.de>
2023-01-04 15:51:55 +0100

    lstclear works no

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: fvon-nag <fvon-nag@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/21 12:58:10 by fvon-nag          #+#    #+#             */
-/*   Updated: 2022/12/21 14:42:48 by fvon-nag         ###   ########.fr       */
+/*   Updated: 2023/01/04 15:50:19 by fvon-nag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,17 @@
 void	ft_lstclear(t_list **lst, void (*del)(void*))
 {
 	t_list	*current;
-	t_list	*lastcurrent;
+	t_list	*temp;
 
-	if (lst == NULL)
+	if (*lst == NULL)
 		return ;
 	current = *lst;
 	while (current != NULL)
 	{
+		temp = current->next;
 		del(current->content);
-		if (current->next != NULL)
-		{
-			lastcurrent = current;
-			current = current->next;
-			del(lastcurrent);
-		}
-		else
-		{
-			del(current);
-			*lst = NULL;
-			return ;
-		}
+		free(current);
+		current = temp;
 	}
+	*lst = NULL;
 }
