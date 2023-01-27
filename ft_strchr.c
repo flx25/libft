39acbd38407c3c6533bc8b3d61811d08643a9944
Felix Von nagel <fvon-nag@c2s16d91.42wolfsburg.de>
2023-01-27 13:55:38 +0100

    updated strchr.c

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,25 +6,26 @@
 /*   By: fvon-nag <fvon-nag@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 09:07:12 by fvon-nag          #+#    #+#             */
-/*   Updated: 2023/01/11 13:27:31 by fvon-nag         ###   ########.fr       */
+/*   Updated: 2023/01/27 13:54:16 by fvon-nag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
+#include "libft.h"
 
-char	*ft_strchr(char *c, int i)
+char	*ft_strchr(const char *s, int i)
 {
 	int	j;
 
 	j = 0;
-	if (!c)
+	if (!s)
 		return (NULL);
 	if (i == '\0')
-		return ((char *)&c[ft_strlen(c)]);
-	while (c[j] != '\0')
+		return ((char *)&s[ft_strlen(s)]);
+	while (s[j] != '\0')
 	{
-		if (c[j] == (char) i)
-			return ((char *)&c[j]);
+		if (s[j] == (char) i)
+			return ((char *)&s[j]);
 		j++;
 	}
 	return (NULL);
